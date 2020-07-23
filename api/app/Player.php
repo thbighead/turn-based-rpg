d3bec22f78913d90d67ece345a317058655ddb79
Thales Nathan <thcmatias@gmail.com>
2020-07-23 11:58:56 -0300

    Changing Model class extension
    We shall use this class from our MongoDB package

--- api/app/Player.php
+++ api/app/Player.php
@@ -5,7 +5,7 @@ namespace App;
 use Illuminate\Auth\Authenticatable;
 use Illuminate\Contracts\Auth\Access\Authorizable as AuthorizableContract;
 use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
-use Illuminate\Database\Eloquent\Model;
+use Jenssegers\Mongodb\Eloquent\Model;
 use Laravel\Lumen\Auth\Authorizable;
 
 class Player extends Model implements AuthenticatableContract, AuthorizableContract
