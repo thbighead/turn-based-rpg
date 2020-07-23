e25e4a38107d6104b9da3d488f421e7cf2546b0d
Thales Nathan <thcmatias@gmail.com>
2020-07-22 22:55:16 -0300

    Configuring database correctly
    Now we can do it correctly

--- api/config/database.php
+++ api/config/database.php
@@ -1,15 +1,13 @@
 <?php
 
 return [
-    'mongodb' => [
-        'driver' => 'mongodb',
-        'host' => env('DB_HOST', '127.0.0.1'),
-        'port' => env('DB_PORT', 27017),
-        'database' => env('DB_DATABASE'),
-        'username' => env('DB_USERNAME'),
-        'password' => env('DB_PASSWORD'),
-        'options' => [
-            'database' => env('DB_AUTHENTICATION_DATABASE', 'admin'), // required with Mongo 3+
+    'default' => 'mongodb',
+    'connections' => [
+        'mongodb' => [
+            'driver' => 'mongodb',
+            'dsn' => env('DB_DSN'),
+            'database' => env('DB_DATABASE'),
         ],
     ],
+    'migrations' => 'migrations',
 ];
