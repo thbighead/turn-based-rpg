49bc0a1aa808658a27a65b733d8a6c7194fffc48
Thales Nathan <thcmatias@gmail.com>
2020-07-25 13:50:30 -0300

    Creating pages
    Now all we must do is control everything with JS!

---
+++ api/resources/views/includes/battle.blade.php
@@ -0,0 +1,11 @@
+<article>
+    <section>
+        <span>Player name:</span> <span></span>
+    </section>
+    <section>
+        <span>Orc:</span> <span></span>
+    </section>
+    <section>
+        <p></p>
+    </section>
+</article>
---
+++ api/resources/views/includes/login.blade.php
@@ -0,0 +1,13 @@
+<article>
+    <section>
+        <form>
+            <label for="player_name">Choose a nickname for you!</label>
+            <input type="text" id="player_name">
+            <button>OK</button>
+        </form>
+    </section>
+
+    <section>
+        <button>Play Game!</button>
+    </section>
+</article>
