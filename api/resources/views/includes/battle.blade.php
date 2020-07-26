ac35a4090d800aab7731f01f86a914a840f59edf
Thales Nathan <thcmatias@gmail.com>
2020-07-26 14:52:28 -0300

    Finishing battle!
    Now our front works properly and do the entire battle functionality!

--- api/resources/views/includes/battle.blade.php
+++ api/resources/views/includes/battle.blade.php
@@ -1,9 +1,34 @@
 <article id="battle_component" class="hidden">
-    <section id="hit_points">
-        <p><span>Player name:</span> <span id="player_hp"></span>/<span id="max_player_hp"></span></p>
-        <p><span>Orc:</span> <span id="enemy_hp"></span>/<span id="max_enemy_hp"></span></p>
+    <section id="characters" data-filled="false">
+        <div id="player" class="card hidden">
+            <p><span class="name">Player name:</span> (<span class="race"></span>)</p>
+            <p>HP: <span class="current-hp"></span>/<span class="max-hp"></span></p>
+            <p>Strength: <span class="strength"></span></p>
+            <p>Agility: <span class="agility"></span></p>
+            <div class="card equipment">
+                <p class="name"></p>
+                <p>Attack bonus: <span class="attack-bonus"></span></p>
+                <p>Defense bonus: <span class="defense-bonus"></span></p>
+                <p>Dice: d<span class="dice-faces"></span></p>
+            </div>
+        </div>
+        <button id="fight">Fight!</button>
+        <div id="enemy" class="card hidden">
+            <p><span class="name">Orc:</span> (<span class="race"></span>)</p>
+            <p>HP: <span class="current-hp"></span>/<span class="max-hp"></span></p>
+            <p>Strength: <span class="strength"></span></p>
+            <p>Agility: <span class="agility"></span></p>
+            <div class="card equipment">
+                <p class="name"></p>
+                <p>Attack bonus: <span class="attack-bonus"></span></p>
+                <p>Defense bonus: <span class="defense-bonus"></span></p>
+                <p>Dice: d<span class="dice-faces"></span></p>
+            </div>
+        </div>
     </section>
     <section>
+        <button id="play_again" class="hidden">Play Again!</button>
+        <small id="battle_errors" class="hidden"></small>
         <p id="log"></p>
     </section>
 </article>
